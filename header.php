0fb1cd2257d0d69de086eebf9b90fdc34b0eae25
melissacabral <melissacabral@gmail.com>
2014-10-08 19:53:46 -0700

    :thumbsup: passing theme check

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
 	<meta charset="utf-8" />
 	<meta name="description" content="A description about your site" />
-	<title><?php bloginfo('name'); ?> - <?php bloginfo('description'); ?></title>
-	<link rel="stylesheet" href="path/to/css/reset.css" media="screen" />
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" media="screen" />
+	<title><?php wp_title(); ?></title>
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/normalize.css" media="screen" />
+	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" media="screen" />
 
 <!--[if IE]>
 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -13,26 +13,22 @@
 	<?php wp_head(); //hook. necessary for plugin css and js to work. ?>
 </head>
 
-<body class="home">
+<body <?php body_class(); ?>>
 	<header>
 		<h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
 		<h2><?php bloginfo('description'); ?></h2>
 
 		<?php get_search_form() ?>
 		
-		<ul class="utilities">
-			<?php wp_list_pages( array(
-					'title_li' 	=> '',
-					'include'	=> '2, 146',
-					) ); ?>
-		</ul>
-		<nav>
-			<ul class="nav">
-				<?php wp_list_pages( array(
-						'title_li'	=>	'',
-						'depth' 	=>	1,
-						'exclude' 	=>	'2, 146',
-				) ); ?>
-			</ul>
-		</nav>
+		<?php wp_nav_menu( array(
+			'theme_location' => 'utilities',				
+			'container' => false,				
+			'fallback_cb' => '',				
+		)); ?>
+
+		<?php wp_nav_menu( array(
+			'theme_location' => 'main_menu',				
+			'container' => 'nav',				
+			'fallback_cb' => '',				
+		)); ?>
 	</header>
--- index.php
+++ index.php
@@ -9,9 +9,16 @@
 			while( have_posts() ):
 			the_post(); ?>
 
-		<article id="post-<?php the_ID(); ?>" class="post">
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			<h2 class="entry-title"> <a href="<?php the_permalink(); ?>"> 
-				<?php the_title(); ?> </a></h2>
+				<?php the_title(); ?> </a></h2>			
+
+			<div class="entry-content">
+				<?php the_post_thumbnail() ?>
+				<?php the_content(); ?>
+				<?php wp_link_pages(  ); ?>
+			</div>
+
 			<div class="postmeta"> 
 				<span class="author"> Posted by: <?php the_author(); ?> </span>
 				<span class="date"> <?php the_date(); ?> </span>
@@ -19,13 +26,15 @@
 				<span class="categories"> <?php the_category(); ?> </span>
 				<span class="tags"><?php the_tags(); ?></span> 
 			</div><!-- end postmeta -->
-
-			<div class="entry-content">
-				<?php the_content(); ?>
-			</div>
 		</article>
 
+		 <?php comments_template(); ?>
+
 		<?php endwhile; //have posts ?>
+		<section class="pagination">
+			<?php next_posts_link();
+			 previous_posts_link();  ?>
+		</section>
 
 	<?php endif; //have posts ?>
 
